41e6b66587b928d85a8ff241d630106119b023b2
Wasiuk <bartlomiej.wasiuk@capgemini.com>
2020-12-16 18:22:53 +0100

    Hello World controller

---
+++ src/main/java/com/wasiuk/demo/helloworld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.wasiuk.demo.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+    	return "Hello World!";
+    }
+    
+    @GetMapping("/envs")
+    public String getEnvs() {
+    	StringBuilder sb = new StringBuilder();
+    	
+    	for(String key : System.getenv().keySet()) {
+    		sb.append(key + "=" + System.getenv().get(key) + "<br>");
+    	}
+    	
+        return sb.toString();
+    }
+}
