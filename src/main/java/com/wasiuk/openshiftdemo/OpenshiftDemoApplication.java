0c360e8fade39c6ec1d94cdd573208271f58607e
Wasiuk <bartlomiej.wasiuk@capgemini.com>
2020-12-16 21:58:08 +0100

    Changing nature to maven

--- src/main/java/com/wasiuk/openshiftdemo/OpenshiftDemoApplication.java
+++ src/main/java/com/wasiuk/openshiftdemo/OpenshiftDemoApplication.java
@@ -1,13 +1,13 @@
-package com.wasiuk.demo;
+package com.wasiuk.openshiftdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class OpenshiftDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
 	}
 
 }
