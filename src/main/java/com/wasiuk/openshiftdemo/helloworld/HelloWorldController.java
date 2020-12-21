40dec3fe4b68d952f121d8847a6c89d67802d652
Wasiuk <bartlomiej.wasiuk@capgemini.com>
2020-12-21 15:22:19 +0100

    New version - webhook test

--- src/main/java/com/wasiuk/openshiftdemo/helloworld/HelloWorldController.java
+++ src/main/java/com/wasiuk/openshiftdemo/helloworld/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-    	return "Hello World! v4";
+    	return "Hello World! v5";
     }
     
     @GetMapping("/envs")
