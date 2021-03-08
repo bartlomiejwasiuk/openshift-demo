8e22fa3d65d6553d3117cd5984804e06e2d33fa6
Wasiuk <bartlomiej.wasiuk@capgemini.com>
2021-03-08 14:11:43 +0100

    New version - v6

--- src/main/java/com/wasiuk/openshiftdemo/helloworld/HelloWorldController.java
+++ src/main/java/com/wasiuk/openshiftdemo/helloworld/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-    	return "Hello World! v5";
+    	return "Hello World! v6";
     }
     
     @GetMapping("/envs")
